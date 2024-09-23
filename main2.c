45e235a348d7f9d9daa2526bcba6117b5d0180e5
Louis Tsai <louis@mct.com.tw>
2024-09-23 16:30:18 +0800

    debug

--- main2.c
+++ main2.c
@@ -2032,7 +2032,7 @@ void create_working_thread(int busid ,int devid)
 //	if(foo != NULL)
 	pthread_mutex_destroy(&foo);
 	pthread_mutex_destroy(&usbctrl_lock);
-	DEBUG_PRINT("leave create_wording_thread ! pid=%d tid=%d\n", getpid(),  pthread_self());
+	DEBUG_PRINT("leave create_wording_thread ! pid=%d\n", getpid();
 	//exit(1);
 }
 
